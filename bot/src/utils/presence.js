4d652938bb926de0584b8509bd9e6df17db3f4c6
Leandro Lourenço <105615180+leanndrocabral@users.noreply.github.com>
2023-06-18 16:25:21 -0300

    Update presence.js

--- bot/src/utils/presence.js
+++ bot/src/utils/presence.js
@@ -3,27 +3,31 @@ class Presence {
   async verifyPresence(command, interaction) {
     const { member, client, guildId } = interaction;
 
-    const queue = client.distube.getQueue(guildId);
-    const voiceChannelId = queue?.voiceChannel?.id;
-
     const memberVoiceChannel = member.voice.channel;
+    const clientVoiceChannel = client.distube.voices.get(guildId);
 
     switch (true) {
       case !memberVoiceChannel:
-        await interaction.reply("Você não está em nenhum canal de voz do servidor.");
-        break
+        await interaction.reply(
+          "Você não está em nenhum canal de voz do servidor."
+        );
+        break;
 
-      case !voiceChannelId && interaction.commandName === "play":
+      case !clientVoiceChannel && interaction.commandName === "play":
         await command.execute(interaction);
-        break
+        break;
 
       case client.voice.adapters.size < 1:
-        await interaction.reply("Eu não estou em nenhum canal de voz do servidor.");
-        break
+        await interaction.reply(
+          "Eu não estou em nenhum canal de voz do servidor."
+        );
+        break;
 
-      case voiceChannelId !== memberVoiceChannel.id:
-        await interaction.reply("Não é possível utilizar este comando pois estou em outro canal de voz.");
-        break
+      case clientVoiceChannel.channelId !== memberVoiceChannel.id:
+        await interaction.reply(
+          "Não é possível utilizar este comando pois estou em outro canal de voz."
+        );
+        break;
 
       default:
         await command.execute(interaction);
