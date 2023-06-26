17e1510b094e472941d7362a8e5df6e779efe52c
leanndro <leanndrocabral@gmail.com>
2023-06-26 17:29:47 -0300

    create temperature command

--- bot/src/utils/embeds.js
+++ bot/src/utils/embeds.js
@@ -3,7 +3,6 @@ const {EmbedBuilder} = require('discord.js');
 class Embeds {
   wallEImage = 'https://avatarfiles.alphacoders.com/197/197092.jpg';
 
-
   songInfo(name, thumbnail, url, views, formattedDuration) {
     return new EmbedBuilder()
         .setColor(0xed4245)
